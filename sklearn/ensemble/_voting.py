881a7bf41ffe049a9470304d40549242f12bc007
wullli <>
2024-05-02 16:23:22 +0200

    Remove check for 1D since we support multi-dimensional now

--- sklearn/ensemble/_voting.py
+++ sklearn/ensemble/_voting.py
@@ -44,7 +44,6 @@ from ..utils.validation import (
     _check_feature_names_in,
     _deprecate_positional_args,
     check_is_fitted,
-    column_or_1d,
 )
 from ._base import _BaseHeterogeneousEnsemble, _fit_single_estimator
 
@@ -691,7 +690,6 @@ class VotingRegressor(RegressorMixin, _BaseVoting):
             Fitted estimator.
         """
         _raise_for_params(fit_params, self, "fit")
-        y = column_or_1d(y, warn=True)
         if sample_weight is not None:
             fit_params["sample_weight"] = sample_weight
         return super().fit(X, y, **fit_params)
